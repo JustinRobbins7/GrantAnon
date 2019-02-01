0053eb7724fd9d874fd5988ce51a58e8f28dea87
JustinRobbins7 <robbinsju@gmail.com>
2019-02-01 16:59:28 -0600

    Commiting changes to player join menu, started work on GameManagers and level start, imported inital tile sprites

--- Assets/Project Assets/Scripts/Player/PlayerControllerAssigner.cs
+++ Assets/Project Assets/Scripts/Player/PlayerControllerAssigner.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class PlayerControllerAssigner : MonoBehaviour
 {
     public int MaxPlayers = 4;
+    public GameObject ReadyScreen = null;
+    public TestPlayerController PlayerPrefab = null;
 
     public Text[] SignInTexts;
     private bool[] ActivePlayers;
     private bool[] ReadyPlayers;
+    bool AllPlayersReady = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +24,26 @@ public class PlayerControllerAssigner : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        for (int i = 0; i < SignInTexts.Length; i++)
+        if (ReadyScreen != null && ReadyScreen.active)
         {
-            CheckPlayerInput(i + 1);
+            AllPlayersReady = true;
+            for (int i = 0; i < SignInTexts.Length; i++)
+            {
+                CheckPlayerInput(i + 1);
+                if (!ReadyPlayers[i] && ActivePlayers[i])
+                {
+                    AllPlayersReady = false;
+                }
+
+                if (AllPlayersReady)
+                {
+                    if (Input.GetButtonDown("PAll_Start"))
+                    {
+                        StartLevel();
+                    }
+                    //ReadyScreen.active = false;
+                }
+            }
         }
     }
 
@@ -33,33 +53,52 @@ public class PlayerControllerAssigner : MonoBehaviour
 
         if (Input.GetButtonDown("P" + ControllerNum.ToString() + "_X"))
         {
+            Debug.Log("P" + ControllerNum.ToString() + "_X Pressed!");
             if (ActivePlayers[index])
             {
-                ActivePlayers[index] = true;
-                SignInTexts[index].text = "Press X to ready up!";
-            }
-            else
-            {
+                Debug.Log("Player " + ControllerNum + " Ready!");
                 ReadyPlayers[index] = true;
                 SignInTexts[index].text = "Player " + ControllerNum.ToString() + " Ready!";
+            }
+            else 
+            {
+                Debug.Log("Player " + ControllerNum + " Joined!");
+                ActivePlayers[index] = true;
+                SignInTexts[index].text = "Press X to ready up!";
             }
         }
 
         if (Input.GetButtonDown("P" + ControllerNum.ToString() + "_Cir"))
         {
+            Debug.Log("P" + ControllerNum.ToString() + "_Cir Pressed!");
             if (ActivePlayers[index])
             {
                 if (ReadyPlayers[index])
                 {
+                    Debug.Log("Player " + ControllerNum + " Unreadied!");
                     ReadyPlayers[index] = false;
                     SignInTexts[index].text = "Press X to ready up!";
                 }
                 else
                 {
+                    Debug.Log("Player " + ControllerNum + " Unjoined!");
                     ActivePlayers[index] = false;
                     SignInTexts[index].text = "Press A to join the game!";
                 }
             }
         }
+    }
+
+    void StartLevel()
+    {
+        for (int i = 0; i < ReadyPlayers.Length; i++)
+        {
+            if (ReadyPlayers[i])
+            {
+                //Spawn Player
+            }
+        }
+
+        gameObject.active = false;
     }
 }

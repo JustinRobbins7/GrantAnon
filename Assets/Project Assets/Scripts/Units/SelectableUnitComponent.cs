790278aa3d551c6bbd6b415a8952e5f4d5c420c6
JustinRobbins7 <robbinsju@gmail.com>
2019-02-24 20:03:14 -0600

    pre .gitignore fix

---
+++ Assets/Project Assets/Scripts/Units/SelectableUnitComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableUnitComponent : MonoBehaviour
+{
+    private bool isSelected = false;
+    private float radius = .5f;
+
+    public void setIsSelected(bool isSelected) {
+        // If no change then don't update
+        if (this.isSelected == isSelected) {
+            return;
+        }
+
+        if (this.isSelected) {
+            
+            // TODO: Hide gameObject that shows that unit is selected
+            gameObject.GetComponent<CircleDraw>().DestroyLineRenderer();
+
+            this.isSelected = false;
+
+        } else {
+            Debug.Log("Called");
+            // TODO: Show gameObject that shows that unit is selected
+            gameObject.GetComponent<CircleDraw>().SetRadius(radius);
+            if (!gameObject.GetComponent<CircleDraw>().HasLineRenderer()) {
+                gameObject.GetComponent<CircleDraw>().InitializeLineRenderer();
+                gameObject.GetComponent<CircleDraw>().UpdateCircleDraw();
+            }
+
+            this.isSelected = true;
+        }
+    }
+}

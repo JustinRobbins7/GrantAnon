38bf1fdbeb97a49247039f3ab2a681ac6b092ffd
Tristan Martin <tristanmartin2016@gmail.com>
2019-03-20 20:12:26 -0500

    Removed unneeded commits

--- Assets/Project Assets/Scripts/Units/Unit.cs
+++ Assets/Project Assets/Scripts/Units/Unit.cs
@@ -15,7 +15,6 @@ public class Unit : MonoBehaviour, IMoveable, ISelectable
     private float radius = .5f;
 
     void Update() {
-        // UpdateMovement();
         if (selected && targetIndex > 0) {
             gameObject.UpdateCircleDraw(radius);
         }
@@ -23,29 +22,7 @@ public class Unit : MonoBehaviour, IMoveable, ISelectable
 
     public void Move(Vector2 target) {
         PathRequestManager.RequestPath(GetComponent<Rigidbody2D>().position, target, OnPathFound);
-        // path = FindObjectOfType<Pathfinding>().FindPath(GetComponent<Rigidbody2D>().position, target);
     }
-
-    /* private void UpdateMovement() {
-        if (path != null && path.Count > 0) {
-            gameObject.UpdateCircleDraw(radius);
-
-            Vector2 targetPos = FindObjectOfType<AStarGrid>().NodeToWorldPosition(path[0]);
-            Vector2 currPos = transform.position;
-
-            if (Math.Round(targetPos.x, 1) == Math.Round(currPos.x, 1) && Math.Round(targetPos.y, 1) == Math.Round(currPos.y, 1)) {
-                path.RemoveAt(0);
-                if (path.Count > 0) {
-                    targetPos = FindObjectOfType<AStarGrid>().NodeToWorldPosition(path[0]);
-                } else {
-                    return;
-                }
-            }
-
-            Vector2 direction = (targetPos - currPos).normalized;
-            GetComponent<Rigidbody2D>().MovePosition(currPos + direction * movementSpeed * Time.deltaTime);
-        }
-    }*/
 
     private void OnPathFound(Vector2[] path, bool success) {
         if (success) {

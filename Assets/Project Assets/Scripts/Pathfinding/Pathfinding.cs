5ecf674354e1c0982d0cddbe308c6593035c0ea7
Tristan Martin <tristanmartin2016@gmail.com>
2019-03-20 19:12:54 -0500

    Changed unit movement to use a Coroutine instead of the update method

--- Assets/Project Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Project Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -1,53 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
     AStarGrid grid;
+    PathRequestManager requestManager;
 
     void Awake() {
         grid = GetComponent<AStarGrid>();
+        requestManager = GetComponent<PathRequestManager>();
     }
 
-    public List<Node> FindPath(Vector2 startPos, Vector2 endPos) {
+    public void StartFindPath(Vector2 startPos, Vector2 targetPos) {
+        StartCoroutine(FindPath(startPos, targetPos));
+    }
+
+    IEnumerator FindPath(Vector2 startPos, Vector2 endPos) {
+        Vector2[] waypoints = new Vector2[0];
+        bool success = false;
+
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(endPos);
 
-        Heap<Node> openSet = new Heap<Node>(grid.GridSize);
-        HashSet<Node> closedSet = new HashSet<Node>();
-        openSet.Add(startNode);
+        if (startNode.walkable && targetNode.walkable) {
+            Heap<Node> openSet = new Heap<Node>(grid.GridSize);
+            HashSet<Node> closedSet = new HashSet<Node>();
+            openSet.Add(startNode);
 
-        while (openSet.Count > 0) {
-            Node currentNode = openSet.RemoveFirst();
-            closedSet.Add(currentNode);
+            while (openSet.Count > 0) {
+                Node currentNode = openSet.RemoveFirst();
+                closedSet.Add(currentNode);
 
-            if (currentNode == targetNode) {
-                return RetracePath(startNode, targetNode);
-            }
-
-            foreach (Node neighbor in grid.GetNeighbors(currentNode)) {
-                if (!neighbor.walkable || closedSet.Contains(neighbor)) {
-                    continue;
+                if (currentNode == targetNode) {
+                    success = true;
+                    break;
                 }
 
-                int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
-                if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor)) {
-                    neighbor.gCost = newMovementCostToNeighbor;
-                    neighbor.hCost = GetDistance(neighbor, targetNode);
-                    neighbor.parent = currentNode;
+                foreach (Node neighbor in grid.GetNeighbors(currentNode)) {
+                    if (!neighbor.walkable || closedSet.Contains(neighbor)) {
+                        continue;
+                    }
 
-                    if (!openSet.Contains(neighbor)) {
-                        openSet.Add(neighbor);
+                    int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+                    if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor)) {
+                        neighbor.gCost = newMovementCostToNeighbor;
+                        neighbor.hCost = GetDistance(neighbor, targetNode);
+                        neighbor.parent = currentNode;
+
+                        if (!openSet.Contains(neighbor)) {
+                            openSet.Add(neighbor);
+                        } else {
+                            openSet.UpdateItem(neighbor);
+                        }
                     }
                 }
             }
         }
 
-        return null;
+        yield return null;
+
+        if (success) {
+            waypoints = RetracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, success);
     }
 
-    List<Node> RetracePath(Node startNode, Node endNode) {
+    Vector2[] RetracePath(Node startNode, Node endNode) {
         List<Node> path = new List<Node>();
         Node currentNode = endNode;
         
@@ -55,9 +75,25 @@ public class Pathfinding : MonoBehaviour
             path.Add(currentNode);
             currentNode = currentNode.parent;
         }
-        path.Reverse();
+        Vector2[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
 
-        return path;
+        return waypoints;
+    }
+
+    Vector2[] SimplifyPath(List<Node> path) {
+        List<Vector2> waypoints = new List<Vector2>();
+        Vector2 directionOld = Vector2.zero;
+
+        for (int i = 1; i < path.Count; i++) {
+            Vector2 directionNew = new Vector2(path[i - 1].gridCol - path[i].gridCol, path[i - 1].gridRow - path[i].gridRow);
+            if (directionNew != directionOld) {
+                waypoints.Add(grid.NodeToWorldPosition(path[i]));
+            }
+            directionOld = directionNew;
+        }
+
+        return waypoints.ToArray();
     }
 
     int GetDistance(Node nodeA, Node nodeB) {
--- Assets/Project Assets/Scripts/Units/Unit.cs
+++ Assets/Project Assets/Scripts/Units/Unit.cs
@@ -7,21 +7,26 @@ public class Unit : MonoBehaviour, IMoveable, ISelectable
 {
     public int OwningControllerNum = 0;
 
-    private List<Node> path = new List<Node>();
+    Vector2[] path;
     [SerializeField] float movementSpeed = 5f;
+    int targetIndex;
 
     private bool selected = false;
     private float radius = .5f;
 
     void Update() {
-        UpdateMovement();
+        // UpdateMovement();
+        if (selected && targetIndex > 0) {
+            gameObject.UpdateCircleDraw(radius);
+        }
     }
 
     public void Move(Vector2 target) {
-        path = FindObjectOfType<Pathfinding>().FindPath(GetComponent<Rigidbody2D>().position, target);
+        PathRequestManager.RequestPath(GetComponent<Rigidbody2D>().position, target, OnPathFound);
+        // path = FindObjectOfType<Pathfinding>().FindPath(GetComponent<Rigidbody2D>().position, target);
     }
 
-    private void UpdateMovement() {
+    /* private void UpdateMovement() {
         if (path != null && path.Count > 0) {
             gameObject.UpdateCircleDraw(radius);
 
@@ -40,6 +45,34 @@ public class Unit : MonoBehaviour, IMoveable, ISelectable
             Vector2 direction = (targetPos - currPos).normalized;
             GetComponent<Rigidbody2D>().MovePosition(currPos + direction * movementSpeed * Time.deltaTime);
         }
+    }*/
+
+    private void OnPathFound(Vector2[] path, bool success) {
+        if (success) {
+            StopCoroutine("FollowPath");
+            this.path = path;
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath() {
+        Vector2 currentWaypoint = path[0];
+        targetIndex = 0;
+
+        while (true) {
+            if (GetComponent<Rigidbody2D>().position == currentWaypoint) {
+                targetIndex++;
+                if (targetIndex >= path.Length) {
+                    targetIndex = 0;
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(GetComponent<Rigidbody2D>().position, currentWaypoint, movementSpeed * Time.deltaTime);
+            gameObject.UpdateCircleDraw(radius);
+            yield return null;
+        }
     }
 
     public void SetSelected(bool selected) {
@@ -49,14 +82,11 @@ public class Unit : MonoBehaviour, IMoveable, ISelectable
         }
 
         if (this.selected) {
-
-            // TODO: Hide gameObject that shows that unit is selected
             gameObject.DestroyCircleDraw();
 
             this.selected = false;
 
         } else {
-            // TODO: Show gameObject that shows that unit is selected
             gameObject.CreateCircleDraw(radius);
 
             this.selected = true;
